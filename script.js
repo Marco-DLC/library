a777f80ca3ba1f91735f3dec6f85d2d599f5f877
Marco-DLC <140775824+Marco-DLC@users.noreply.github.com>
2024-04-10 19:09:41 -0400

    Add function to create CSS Grid for books
    I've added dummy objects in the myLibrary array to test the CSS grid.

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-const myLibrary = [];
+const myLibrary = [
+    {
+        name: 'book1',
+        read: 'not read',
+        author: 'author1'
+    },
+    {
+        name: 'book2',
+        read: 'read',
+        author: 'author2'
+    },
+    {
+        name: 'book3',
+        read: 'read',
+        author: 'author3'
+    }
+];
 
-function Book() {
+// function Book() {
 
+// }
+
+// function addBookToLibrary() {
+
+// }
+
+const bookGrid = document.getElementById('bookGrid');
+
+function createBookGrid(){
+    myLibrary.forEach((book) => {
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('book-card');
+
+        bookCard.textContent = Object.entries(book).join(' ', '');
+        bookGrid.appendChild(bookCard);
+    })
 }
 
-function addBookToLibrary() {
-    
-}
+createBookGrid();
